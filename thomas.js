8adf2df8926383317db91f1de43201f38209672d
Thomas Spycher <tsthomas123@gmail.com>
2017-05-23 11:18:12 -0700

    commit

--- thomas.js
+++ thomas.js
@@ -1,20 +1,23 @@
-function game(name, teams, players) {
+function Game(name, teams, players) {
     this.name = name;
     this.teams = teams;
     this.players = players;
-}
 
-function Player(captain, kill, dead) {
-    this.captain = captain;
-    this.kill = kill;
-    this.dead = dead;
 }
 
 
-function team(name, players, status) {
+function Team(name, teamid, players, status) {
     this.teamName = name;
+    this.teamid = teamid;
     this.players = players;
     this.alive = status;
 }
 
 
+function Player(name, captain, id, kills, status) {
+    this.name = name;
+    this.id = id;
+    this.captain = captain;
+    this.alive = status;
+    this.kills = kills;
+}
