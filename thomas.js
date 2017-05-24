ff94691109d3ce7e2856bc41e336383fee13378c
TheOneAndOnlyKoko <k.natsoulis@gmail.com>
2017-05-24 11:07:45 -0700

    qwerty

--- thomas.js
+++ thomas.js
@@ -61,5 +61,3 @@ function reassignTeam() {
     //once team is dead, assign team who killed them their new team
     //if no more teams are left to assign then they won the game
 }
-
- //kostya is a stupid piece of shit who should go kill himself in some shitty Denny's bathroom. :)
