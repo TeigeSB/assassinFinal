52673a4533f33a11ae94dc42995d6fcc3a1f0d0c
Thomas Spycher <tsthomas123@gmail.com>
2017-05-25 11:18:56 -0700

    commit

--- data.js
+++ data.js
@@ -1,6 +1,29 @@
-/**
- * Created by h205p3 on 5/25/17.
- */
+function Game(name, teams, players) {
+    this.name = name;
+    this.teams = teams;
+    this.players = players;
+}
+
+function Player(captain, kill, dead) {
+    this.captain = captain;
+    this.kill = kill;
+    this.dead = dead;
+}
+
+
+function Team(name, players, status) {
+    this.teamName = name;
+    this.players = players;
+    this.alive = status;
+}
+
+
+function Rules(select) {
+    this.select = select;
+}
+
+
+
 //this will save an array of objects to a local storage
 function setData(array, name) {
     var json = JSON.stringify(array);
@@ -15,25 +38,13 @@ function getData(name) {
 }
 
 
+
 function startGame() {
     //make data
     var player1 = {name: "Kostya", isAlive: false};
     var player2 = {name: "Teige", isAlive: true};
 
     var players = [player1, player2];
-
-    setData(players,"data");
-
-}
-
-
-
-function getNextVictim() {
-    var players = getData("data");
-
-    //iterate through players array, find an alive person
-
-    ///change to assigned, etc
 
     setData(players,"data");
 
--- kostya.js
+++ kostya.js
@@ -1,37 +1,3 @@
-function Name(name, teams, players) {
-    this.name = name;
-    this.teams = teams;
-    this.players = players;
-}
-
-function Player(captain, kill, dead) {
-    this.captain = captain;
-    this.kill = kill;
-    this.dead = dead;
-}
-
-
-function Team(name, players, status) {
-    this.teamName = name;
-    this.players = players;
-    this.alive = status;
-}
-
-function Rules(select) {
-    this.select = select;
-}
-
-function startGame() {
-    //assign every team a target of a different team
-}
-
-
-function kill(player,playerDead) {
-    //change player status to dead
-    //add 1 to the players kill count
-}
-
-
 function eliminateTeam() {
     //once player dies see if whole team is dead
     //if whole team or team captain is dead change team status to dead
@@ -39,9 +5,6 @@ function eliminateTeam() {
 }
 
 
-function reassignTeam() {
-    //once team is dead, assign team who killed them their new team
-    //if no more teams are left to assign then they won the game
-}
+
 
 
