40b4d5fb8d85b5b2c31aae406412e4e72403bc2c
TeigeSB <teigebradford@students.berkeley.net>
2017-05-26 11:21:51 -0700

    wowww

--- teige.js
+++ teige.js
@@ -1,3 +1,14 @@
 /**
  * Created by h205p3 on 5/22/17.
  */
+
+function onLoad() {
+    $("#page2go").hide
+}
+
+function showP2() {
+    if($("#password1") !== "" && $("#username") !== "") {
+        $("#page2go").show
+    }
+}
+
