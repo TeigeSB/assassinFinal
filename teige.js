1f35b04a9fabb6246b4b2b579b24a26fe7862e72
TeigeSB <teigebradford@students.berkeley.net>
2017-06-05 12:23:45 -0700

    adfgsdsfdfgdfdfdsfa

--- teige.js
+++ teige.js
@@ -2,13 +2,34 @@
  * Created by h205p3 on 5/22/17.
  */
 
-function onLoad() {
-    $("#page2go").hide
-}
+userNames = [];
+passWords = [];
 
-function showP2() {
-    if($("#password1") !== "" && $("#username") !== "") {
-        $("#page2go").show
+$("#login").click(function() {
+    for (var i = 0; i < userNames.length; i++) {
+        if ($("#clear-demo") !== userNames[i]) {
+            setData(userNames, $("#clear-demo"));
+            console.log($("#clear-demo"));
+        } else if ($("#username") == userNames[i] && $("#password") == passWords[i]) {
+
+        }
+
     }
+});
+
+
+function saveLogin () {
+    for (var i = 0; i < userNames.length; i++) {
+        if ($("#clear-demo") !== userNames[i]) {
+            setData(userNames, $("#clear-demo"));
+            console.log($("#clear-demo"));
+        } else if ($("#username") == userNames[i] && $("#password") == passWords[i]) {
+
+        } else {
+            console.log($("#clear-demo"))
+        }
+    }
+        $( "#clear-demo" ).textinput();
+    console.log($("#clear-demo"))
 }
 
