746457d56910efd610d67142f589755bebd0d6f6
TeigeSB <teigebradford@students.berkeley.net>
2017-06-08 10:53:52 -0700

    who killed who died

--- teige.js
+++ teige.js
@@ -4,29 +4,22 @@ passWords = ["pw"];
 
 function saveLogin () {
 
-    var user = getData('hey');
-    var pass = getData(passWords);
-    console.log(user);
     var inputPass= $("#clear-demo").val();
     var inputName = $("#password1").val();
 
+    console.log();
 
-
-    for (var i = 0; i < user.length; i++) {
-        if (inputPass !== getData(inputPass) && inputName !== getData(inputName)) {
+    if (inputPass !== getData(inputPass) && inputName !== getData(inputName)) {
             setData(userNames, inputPass);
             setData(passWords, inputName);
+            prompt('Username and Password are incorrect, would you like to create an account?');
             console.log(inputPass);
-        } else if (inputPass !== getData(inputPass)) {
+    } else if (inputPass !== getData(inputPass)) {
             prompt('Your Username is incorrect!')
-        }
-            else if (inputPass == getData(inputPass) && inputName == getData(inputName)) {
+    } else if (inputPass == getData(inputPass) && inputName == getData(inputName)) {
             console.log("HI")
-        } else {
+    } else {
             console.log('NOT WORKING')
         }
-    }
-    console.log(user);
-    console.log(pass)
 }
 
