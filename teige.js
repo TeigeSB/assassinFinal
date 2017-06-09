4dde7a8d3844f9fb8cbcb9d76326272be5a73ea2
TeigeSB <teigebradford@students.berkeley.net>
2017-06-09 10:53:39 -0700

    who killed who died and BITTOPN

--- teige.js
+++ teige.js
@@ -1,25 +1,38 @@
 
-userNames = ["user"];
-passWords = ["pw"];
+userNames = [];
+passWords = [];
 
 function saveLogin () {
 
-    var inputPass= $("#clear-demo").val();
-    var inputName = $("#password1").val();
+    var inputName= $("#clear-demo").val();
+    var inputPass = $("#password1").val();
 
     console.log();
 
-    if (inputPass !== getData(inputPass) && inputName !== getData(inputName)) {
-            setData(userNames, inputPass);
-            setData(passWords, inputName);
-            prompt('Username and Password are incorrect, would you like to create an account?');
-            console.log(inputPass);
-    } else if (inputPass !== getData(inputPass)) {
-            prompt('Your Username is incorrect!')
+    if (inputPass != getData(inputPass) && inputName != getData(inputName)) {
+            var yesNo = confirm('Username and Password are incorrect, would you like to create an account?');
+        if (yesNo) {
+            userNames.push(inputName);
+            passWords.push(inputPass)
+            setData(userNames, inputName);
+            setData(passWords, inputPass);
+            console.log(getData(inputName));
+            console.log(getData(inputPass));
+
+        } else if (!yesNo) {
+            alert('Please enter your username and password correctly.')
+        }
+    } else if (inputPass != getData(inputPass)) {
+            alert('Your Password is incorrect!')
+    } else if (inputName != getData(inputName)) {
+            alert('Your Username is incorrect')
     } else if (inputPass == getData(inputPass) && inputName == getData(inputName)) {
             console.log("HI")
+        console.log(getData(inputName));
+        console.log(getData(inputPass));
     } else {
             console.log('NOT WORKING')
         }
+
 }
 
